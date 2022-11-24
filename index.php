16f4099358b5cfb98248232db0516294ab097254
Kevin Fuerez <kjf.fuerez@yavirac.edu.ec>
2022-11-24 05:41:42 -0500

    Adelantos

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
-<html lang = "en">
-	<head>
-		<title>Gestor Free | Fuente Web</title>
-		<meta charset = "utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel = "stylesheet" type="text/css" href="admin/css/bootstrap.css" />
-		<link rel = "stylesheet" type="text/css" href="admin/css/style.css" />
-	</head>
+<html lang="en">
+
+<head>
+    <title>Gestor Free | Fuente Web</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="admin/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="admin/css/style.css" />
+</head>
+
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top" style="background-color:blue;">
-		<div class="container-fluid">
-			<label class="navbar-brand" id="title">Sistema Gestor de Archivos Básico</label>
-		</div>
-	</nav>
-	<?php include 'login.php'?>
-	<div id = "footer">
-	</div>
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color:blue;">
+        <div class="container-fluid">
+            <label class="navbar-brand" id="title">Sistema Gestor de Archivos Básico</label>
+        </div>
+    </nav>
+
+    <?php include 'sidebar.php'?>
+    <?php include 'login.php'?>
+    <div id="footer">
+    </div>
 </body>
+
 </html>
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<div id="sidebar">
+    <ul id="menu" class="nav menu">
+        <li><a href="form.php"><i class="glyphicon glyphicon-home"></i> Datos Personales</a></li>
+        <li><a href="student_profile.php"><i class="glyphicon glyphicon-user"></i> Portafolio</a>
+        </li>
+    </ul>
+</div>
